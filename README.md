2092a56817cb1e6b1f8cc000d158d9bea4326c5d
Toni M„ki <maki.toni@gmail.com>
2014-12-06 21:33:18 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# MCC Mobile
+
+## Setup
+
+These files include all the files of the Eclipse project this app was created with and thus
+can be installed by opening the project in Eclipse and running it with your phone hooked in.
+(Note that you need the ADT plugin for Eclipse: http://developer.android.com/tools/sdk/eclipse-adt.html)
+
+Directions for hooking your phone as a test device: http://developer.android.com/tools/device.html
+
+You can also use the ADB command line tool to build and install the project:
+http://developer.android.com/tools/help/adb.html
+
+## Using
+
+This is mobile app for the MCC Contactlist application.
+The app is very simple to use. The main menu consists of following options:
+
+1. Get Contacts
+2. Create Contact
+3. Delete Contact
+4. Import Contact
+5. Export Contact
+
+
+###Get Contact
+Lists all contacts pulled from the MCC web application. Touching a contact
+lists all information for that specific contact.
+
+###Create contact
+Allows you to create a new contact that is then pushed to the MCC web application
+
+###Delete Contact
+Touch a contact to delete them. They are also deleted from the MCC web application
+
+###Import Contact
+Import a contact from your phone into the MCC application. Choosing this option will open
+your phones contact manager and choosing a contact will import it into the MCC application.
+
+###Export Contact
+Touching a contact will save that contact into your devices contact list.
--- gen/com/mcc/mccmobile/R.java
+++ gen/com/mcc/mccmobile/R.java
@@ -1579,6 +1579,7 @@ containing a value of this type.
         public static final int dialog_fixed_width_minor=0x7f07000d;
         public static final int disabled_alpha_material_dark=0x7f070001;
         public static final int disabled_alpha_material_light=0x7f070000;
+        public static final int navigation_drawer_width=0x7f07002d;
     }
     public static final class drawable {
         public static final int abc_ab_share_pack_holo_dark=0x7f020000;
@@ -1632,7 +1633,9 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
+        public static final int drawer_shadow=0x7f020033;
+        public static final int ic_drawer=0x7f020034;
+        public static final int ic_launcher=0x7f020035;
     }
     public static final class id {
         public static final int action_bar=0x7f090031;
@@ -1643,47 +1646,58 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f09001f;
         public static final int action_bar_title=0x7f09001e;
         public static final int action_context_bar=0x7f090032;
+        public static final int action_example=0x7f090052;
         public static final int action_menu_divider=0x7f090005;
         public static final int action_menu_presenter=0x7f090006;
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090046;
+        public static final int action_settings=0x7f090051;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
-        public static final int btnDeleteService=0x7f090042;
-        public static final int btnExportContact=0x7f090044;
-        public static final int btnImportContact=0x7f090043;
-        public static final int btnPostService=0x7f090041;
-        public static final int btnStartService=0x7f09003f;
-        public static final int btnStopService=0x7f090040;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
+        public static final int container=0x7f090040;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
+        public static final int device=0x7f09004a;
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
+        public static final int drawer_layout=0x7f09003f;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
+        public static final int email_input=0x7f09004e;
+        public static final int email_label=0x7f090046;
+        public static final int email_view=0x7f090047;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090026;
+        public static final int id_label=0x7f090048;
+        public static final int id_view=0x7f090049;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
+        public static final int info_view=0x7f09004f;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
-        public static final int listview=0x7f090045;
         public static final int middle=0x7f09001d;
+        public static final int name_input=0x7f09004c;
+        public static final int name_label=0x7f090042;
+        public static final int name_view=0x7f090043;
+        public static final int navigation_drawer=0x7f090041;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
+        public static final int phone_input=0x7f09004d;
+        public static final int phone_label=0x7f090044;
+        public static final int phone_view=0x7f090045;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
+        public static final int save_action=0x7f09004b;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
@@ -1699,6 +1713,7 @@ containing a value of this type.
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
         public static final int split_action_bar=0x7f090004;
+        public static final int status_view=0x7f090050;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090027;
@@ -1757,10 +1772,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int fragment_contact_detail=0x7f030018;
+        public static final int fragment_contact_list=0x7f030019;
+        public static final int fragment_contact_save=0x7f03001a;
+        public static final int fragment_navigation_drawer=0x7f03001b;
+        public static final int item_contact=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int global=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1819,14 +1840,34 @@ containing a value of this type.
         /**  Content description for the Toolbar icon used to collapse an expanded action mode. [CHAR LIMIT=NONE] 
          */
         public static final int abc_toolbar_collapse_description=0x7f0a0004;
+        public static final int action_example=0x7f0a001b;
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a0010;
+        public static final int create_contact=0x7f0a001d;
+        public static final int created=0x7f0a0029;
+        public static final int creating=0x7f0a0028;
+        public static final int delete_contact=0x7f0a001e;
         public static final int delete_service=0x7f0a0016;
+        public static final int deleted=0x7f0a0025;
+        public static final int deleting=0x7f0a0024;
+        public static final int email=0x7f0a0022;
         public static final int export_contact=0x7f0a0018;
+        public static final int exported=0x7f0a0026;
+        public static final int exporting=0x7f0a0027;
+        public static final int get_contacts=0x7f0a001c;
         public static final int hello_world=0x7f0a0011;
+        public static final int id=0x7f0a002b;
         public static final int import_contact=0x7f0a0017;
+        public static final int imported=0x7f0a002a;
+        public static final int name=0x7f0a0020;
+        public static final int navigation_drawer_close=0x7f0a001a;
+        public static final int navigation_drawer_open=0x7f0a0019;
+        public static final int phone=0x7f0a0021;
         public static final int post_service=0x7f0a0015;
+        public static final int save=0x7f0a001f;
+        public static final int show_contact=0x7f0a002c;
         public static final int start_service=0x7f0a0013;
+        public static final int status=0x7f0a0023;
         public static final int stop_service=0x7f0a0014;
     }
     public static final class style {
@@ -1840,11 +1881,6 @@ containing a value of this type.
             backward-compatibility can go here.
         
 
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
@@ -1852,10 +1888,6 @@ containing a value of this type.
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f0b00eb;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f0b00ec;
         public static final int Base_TextAppearance_AppCompat=0x7f0b0098;
         public static final int Base_TextAppearance_AppCompat_Body1=0x7f0b00a3;
         public static final int Base_TextAppearance_AppCompat_Body2=0x7f0b00a2;
